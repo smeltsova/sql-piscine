7657b56e97994fe3845aa4dd56c6534415253f8e
Ekaterina <smeltsova.er@gmail.com>
2023-08-28 15:53:10 +0300

    finished day08

---
+++ SQL_beginner.Day08-1/src/ex06/day08_ex06.sql
@@ -0,0 +1,20 @@
+--Session #1
+SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+--Session #2
+SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+--Session #1
+BEGIN;
+--Session #2
+BEGIN;
+--Session #1
+SELECT SUM(rating) FROM pizzeria;
+--Session #2
+SELECT SUM(rating) FROM pizzeria;
+UPDATE pizzeria SET rating = 5 WHERE name = 'Pizza Hut';
+COMMIT;
+--Session #1
+SELECT SUM(rating) FROM pizzeria;
+COMMIT;
+SELECT SUM(rating) FROM pizzeria;
+--Session #2
+SELECT SUM(rating) FROM pizzeria;
